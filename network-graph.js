011716605eaf602bae75d09793cbbfa0ba5387c1
Robert Keizer <robert@keizer.ca>
2011-03-22 22:35:11 -0500

    Basic functionality.

--- network-graph.js
+++ network-graph.js
@@ -7,7 +7,65 @@ var url			= require( "url" );
 var io			= require( "socket.io" );
 var pcap		= require( "pcap" );
 var pcap_session	= pcap.createSession( "", "tcp port 80" );
-console.log( "Listening on " + pcap_session.device_name );
+var httpPort 		= "8080";
+
+console.log( "Starting HTTP server on port " + httpPort );
+
+httpServer = http.createServer( function(req,res){
+        var path = url.parse(req.url).pathname;
+        switch( path ){
+                case '/':
+                        res.writeHead( {'Content-Type': 'text/html'} );
+                        res.write( 'Try /view.html' );
+                        res.end();
+                case '/view.html':
+                        fs.readFile(__dirname+path,function(err,data){
+                                if( err ){
+					return send404(res);
+                                };
+                                res.writeHead( {'Content-Type': 'text/html'} );
+                                res.write( data, 'utf8' );
+                                res.end();
+                        } );
+                        break;
+		case '/jquery.js':
+			fs.readFile(__dirname+path,function(err,data){
+				if( err ){
+					return send404(res);
+				}
+				res.writeHead( {'Content-Type': 'text/javascript'} );
+				res.write( data, 'utf8' );
+				res.end();	
+			} );
+			break;
+                default:
+			send404(res);
+        };
+} );
+
+function send404( res ){
+	res.writeHead(404);
+	res.write('404');
+	res.end();
+}
+
+httpServer.listen(httpPort);
+
+console.log( "Starting up socket.io .." );
+
+var boundIo = io.listen( httpServer );
+boundIo.on( 'connection', function serveClient( client ){
+	client.on( 'message', function(message){
+		console.log( 'Received message of ' + message ) ;
+	} );
+
+	client.on( 'diconnect', function( ){
+		client.broadcast( { announcement: client.sessionId + 'disconnected.' } );
+	} );
+} );
+
+
+console.log( "Monitoring traffic on on " + pcap_session.device_name + ".." );
 
 var blacklistDnsCache = new Array();
 blacklistDnsCache["192.168.1.100"] = 1;
@@ -39,58 +97,7 @@ pcap_session.on( "packet", function( raw_packet ){
 		var nameToPass = packet.link.ip.daddr.toString();
 	}
 
-	
+	// nameToPass contains the ip/dns. Send it to the clients.
+
+	boundIo.broadcast( nameToPass );
 } );
-
-var httpPort = "8080";
-
-console.log( "Starting HTTP server on port " + httpPort );
-
-httpServer = http.createServer( function(req,res){
-
-	var path = url.parse(req.url).pathname;
-
-	switch( path ){
-		case '/':
-			res.writeHead( {'Content-Type': 'text/html'} );
-			res.write( 'Try /view.html' );
-			res.end();
-		case '/view.html':
-			fs.readFile(__dirname+path,function(err,data){
-				if( err ){
-					return send404( res );
-				};
-				res.writeHead( {'Content-Type': 'text/html'} );
-				res.write( data, 'utf8' );
-				res.end();
-			} );
-			break;
-		default:
-			send404(res);
-	};
-} );
-
-var send404 = function( res ){
-	res.writeHead(404);
-	res.write('404');
-	res.end();
-}
-
-httpServer.listen(httpPort);
-
-var io = io.listen( httpServer )
-var buffer = new Array();
-
-io.on( 'connection', function( client ){
-	client.on( 'message', function(message){
-		var msg = { message: [client.sessionId, message] };
-		buffer.push( msg );
-		console.log( sys.inspect( msg ) );
-	} );
-	
-	client.on( 'disconnect', function( ){
-		client.broadcast( { announcement: client.sessionId + 'disconnected.' } );
-	} );
-} );
-
-console.log( sys.inspect( io ) );
