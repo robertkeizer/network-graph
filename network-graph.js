f5d7ad1af2afc4d555e266b06752c3cf7804c154
Robert Keizer <robert@keizer.ca>
2011-03-22 21:48:44 -0500

    Messing with socket.io - trying to mash together

--- network-graph.js
+++ network-graph.js
@@ -1,7 +1,10 @@
 console.log( "Starting up.." );
 var sys			= require( "sys" );
+var fs			= require( "fs" );
 var dns			= require( "dns" );
 var http		= require( "http" );
+var url			= require( "url" );
+var io			= require( "socket.io" );
 var pcap		= require( "pcap" );
 var pcap_session	= pcap.createSession( "", "tcp port 80" );
 console.log( "Listening on " + pcap_session.device_name );
@@ -31,8 +34,63 @@ pcap_session.on( "packet", function( raw_packet ){
 	};
 
 	if( typeof dnsCacheArray[packet.link.ip.daddr.toString()] != 'undefined' ){
-		console.log( dnsCacheArray[packet.link.ip.daddr.toString()] );
+		var nameToPass = dnsCacheArray[packet.link.ip.daddr.toString()];
 	}else{
-		console.log( packet.link.ip.daddr.toString() );
+		var nameToPass = packet.link.ip.daddr.toString();
 	}
+
+	
 } );
+
+var httpPort = "8080";
+
+console.log( "Starting HTTP server on port " + httpPort );
+
+httpServer = http.createServer( function(req,res){
+
+	var path = url.parse(req.url).pathname;
+
+	switch( path ){
+		case '/':
+			res.writeHead( {'Content-Type': 'text/html'} );
+			res.write( 'Try /view.html' );
+			res.end();
+		case '/view.html':
+			fs.readFile(__dirname+path,function(err,data){
+				if( err ){
+					return send404( res );
+				};
+				res.writeHead( {'Content-Type': 'text/html'} );
+				res.write( data, 'utf8' );
+				res.end();
+			} );
+			break;
+		default:
+			send404(res);
+	};
+} );
+
+var send404 = function( res ){
+	res.writeHead(404);
+	res.write('404');
+	res.end();
+}
+
+httpServer.listen(httpPort);
+
+var io = io.listen( httpServer )
+var buffer = new Array();
+
+io.on( 'connection', function( client ){
+	client.on( 'message', function(message){
+		var msg = { message: [client.sessionId, message] };
+		buffer.push( msg );
+		console.log( sys.inspect( msg ) );
+	} );
+	
+	client.on( 'disconnect', function( ){
+		client.broadcast( { announcement: client.sessionId + 'disconnected.' } );
+	} );
+} );
+
+console.log( sys.inspect( io ) );
