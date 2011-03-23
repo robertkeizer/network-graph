ae8bf23c587a4d8ce561ac696af618b8eff81375
Robert Keizer <robert@keizer.ca>
2011-03-22 23:24:07 -0500

    Starting out with protovis.

--- network-graph.js
+++ network-graph.js
@@ -13,35 +13,39 @@ console.log( "Starting HTTP server on port " + httpPort );
 
 httpServer = http.createServer( function(req,res){
         var path = url.parse(req.url).pathname;
-        switch( path ){
-                case '/':
-                        res.writeHead( {'Content-Type': 'text/html'} );
-                        res.write( 'Try /view.html' );
-                        res.end();
-                case '/view.html':
-                        fs.readFile(__dirname+path,function(err,data){
-                                if( err ){
-					return send404(res);
-                                };
-                                res.writeHead( {'Content-Type': 'text/html'} );
-                                res.write( data, 'utf8' );
-                                res.end();
-                        } );
-                        break;
-		case '/jquery.js':
+	switch( path ){
+		case "/":
+			return sendMessage( res, "Try /view.html" );
+			break;
+		case "/view.html":
 			fs.readFile(__dirname+path,function(err,data){
-				if( err ){
+				if(err){
 					return send404(res);
-				}
-				res.writeHead( {'Content-Type': 'text/javascript'} );
-				res.write( data, 'utf8' );
-				res.end();	
+				};
+				sendMessage( res, data );
 			} );
 			break;
-                default:
+		case "/jquery.js":
+		case "/protovis.js":
+			fs.readFile(__dirname+path,function(err,data){
+				if(err){
+					return send404(res);
+				};
+				res.writeHead( 200, {'Content-Type': 'text/javascript'} );
+				res.write( data );
+				res.end();
+			} );
+			break;
+		default:
 			send404(res);
-        };
+	};
 } );
+
+function sendMessage( res, msg ){
+	res.writeHead( 200, {'Content-Type': 'text/html'} );
+	res.write( msg, 'utf8' );
+	res.end();
+}
 
 function send404( res ){
 	res.writeHead(404);
@@ -97,6 +101,7 @@ pcap_session.on( "packet", function( raw_packet ){
 		var nameToPass = packet.link.ip.daddr.toString();
 	}
 
+	console.log( nameToPass );
 	// nameToPass contains the ip/dns. Send it to the clients.
 
 	boundIo.broadcast( nameToPass );
