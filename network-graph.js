61043149a0e42444a7cda76fc4b20c6b42d3e17e
Robert Keizer <robert@keizer.ca>
2011-03-25 12:09:55 -0500

    Small updates.

--- network-graph.js
+++ network-graph.js
@@ -20,25 +20,21 @@ httpServer = http.createServer( function(req,res){
 		var path = '/view.html';
 		fs.readFile(__dirname+path,function(err,data){
 			if(err){ return send404(res); };
-			sendMessage( res, data );
+			sendMessage( res, data, 'text/html' );
 		} );
 
 	// html files..
 	}else if ( path.match( /\.html$/ ) ){
 		fs.readFile(__dirname+path,function(err,data){
 			if(err){ return send404(res); };
-			res.writeHead( 200, {'Content-Type': 'text/html'} );
-			res.write( data );
-			res.end();
+			sendMessage( res, data, 'text/html' );
 		} );
 
 	// javascript files..
 	}else if ( path.match( /\.js$/ ) ){
 		fs.readFile(__dirname+path,function(err,data){
 			if(err){ return send404(res); };
-			res.writeHead( 200, {'Content-Type': 'text/javascript'} );
-			res.write( data );
-			res.end();
+			sendMessage( res, data, 'text/javascript' );
 		} );
 
 	// not found.
@@ -47,8 +43,8 @@ httpServer = http.createServer( function(req,res){
 	};
 } );
 
-function sendMessage( res, msg ){
-	res.writeHead( 200, {'Content-Type': 'text/html'} );
+function sendMessage( res, msg, contentType ){
+	res.writeHead( 200, {'Content-Type': contentType} );
 	res.write( msg, 'utf8' );
 	res.end();
 }
