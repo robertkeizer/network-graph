e71d646051ff4acb7103e899b56fefaf918034f7
Robert Keizer <robert@keizer.ca>
2011-03-30 21:02:52 -0500

    Worked on rewrite..

--- network-graph.js
+++ network-graph.js
@@ -1,128 +1,120 @@
-console.log( "Starting up.." );
+function debug( msg ){
+	console.log( msg );
+}
+
+debug( "Starting up.." );
+
+debug( "Setting defaults and creating variables.." );
+var httpPort	= "8080";
+var filter	= "tcp";
+var interface	= "";
+var pcapSession;
+var ioInstance;
+
+debug( "Including modules.." );
+
 var sys			= require( "sys" );
 var fs			= require( "fs" );
-var dns			= require( "dns" );
 var http		= require( "http" );
 var url			= require( "url" );
 var io			= require( "socket.io" );
 var pcap		= require( "pcap" );
+var arguments		= require( "arguments" );
 
-var httpPort 		= "8080";
-var filter		= "tcp";
+arguments.parse([
+	{ 'name': /^(-h|--help)$/, 'expected': null, 'callback': showHelp },
+	{ 'name': /^(-p|--port)$/, 'expected': /^[0-9]*$/, 'callback': setPort },
+	{ 'name': /^(-i|--interface)$/, 'expected': /[a-zA-Z0-9]*$/, 'callback': setInterface },
+	{ 'name': /^(-f|--filter)$/, 'expected': /^[a-zA-Z.-0-9]*$/, 'callback': setFilter }
+		], main, invalidArgument );
 
-var dnsHostCache		= Array( );
-dnsHostCache['127.0.0.1']	= 'localhost';
-var dnsHostBlacklist		= Array( );
 
-console.log( "Starting HTTP server on port " + httpPort );
-
-httpServer = http.createServer( function(req,res){
-        var path = url.parse(req.url).pathname;
-
-	// No file specified - shove to view.html
-	if( path == '/' ){
-		var path = '/view.html';
-		fs.readFile(__dirname+path,function(err,data){
-			if(err){ return send404(res); };
-			sendMessage( res, data, 'text/html' );
-		} );
-
-	// html files..
-	}else if ( path.match( /\.html$/ ) ){
-		fs.readFile(__dirname+path,function(err,data){
-			if(err){ return send404(res); };
-			sendMessage( res, data, 'text/html' );
-		} );
-
-	// javascript files..
-	}else if ( path.match( /\.js$/ ) ){
-		fs.readFile(__dirname+path,function(err,data){
-			if(err){ return send404(res); };
-			sendMessage( res, data, 'text/javascript' );
-		} );
-
-	// not found.
-	}else{
-		send404(res);
-	};
-} );
-
-function sendMessage( res, msg, contentType ){
-	res.writeHead( 200, {'Content-Type': contentType} );
-	res.write( msg, 'utf8' );
-	res.end();
-}
-
-function send404( res ){
-	res.writeHead(404);
-	res.write('404');
-	res.end();
-}
-
-httpServer.listen(httpPort);
-
-var boundIo = io.listen( httpServer );
-console.log( "Starting up socket.io .." );
-
-var pcap_session	= pcap.createSession( "", filter );
-var tcp_tracker		= new pcap.TCP_tracker( );
-
-pcap_session.on( "packet", function( raw_packet ){
-	tcp_tracker.track_packet( pcap.decode.packet( raw_packet ) );
-} );
-
-console.log( "Monitoring traffic on " + pcap_session.device_name + " with filter of '" + filter + "'.." );
-
-tcp_tracker.on( 'start', function( session ){
-	sendToClients( session, 'start' );
-} );
-
-tcp_tracker.on( 'end', function( session ){
-	sendToClients( session, 'end' );
-} );
-
-function sendToClients( session, status ){
-	// ports change frequently enough, we just want the IP's rather than the full key.
-	var hostSessionKey	= session.key.replace( /:[0-9]*/g, '' );
+function showHelp( end ){
+	var msg	= "Usage:	(-h|--help): This message.\n";
+	msg	+="	(-p|--port): The HTTP port.\n";
+	msg	+="	(-i|--interface): The interface to listen on.\n";
+	msg	+="	(-f|--filter): The filter to utilize when listening on the device.\n";
 	
-	boundIo.broadcast( [ {	current_cap_time: session.current_cap_time, 
-				session_key: hostSessionKey,
-				status: status } ] );
-};
+	debug( msg );
+	end( );
+}
 
-// Grab messages from clients..
-boundIo.on( 'message', function( obj ){
-	if( typeof obj.requestType === 'undefined' ){
-		console.log( "Got invalid message from a client.. " );
+function setPort( end, portToSet ){
+	httpPort	= portToSet;
+	end( );
+}
+
+function setInterface( end, interfaceToListenOn ){
+	interface	= interfaceToListenOn;
+	end( );
+}
+
+function setFilter( end, filterToSet ){
+	filter		= filterToSet;
+	end( );
+}
+
+function invalidArgument( arg, missingValue ){
+	if( missingValue ){
+		debug( "Argument '" + arg + "' is missing." );
 	}else{
-		boundIo.send( doRequest( obj ) );
-	}
-} );
-
-// Parse a clients message..
-
-function doRequest( obj ){
-	switch( obj.requestType ){
-		case "get":
-			switch( obj.what ){
-				case "filter":
-					return filter;
-					break;
-				default:
-					return "Unknown 'what' specified in get request.";
-			}
-			break;
-		case "set":
-			switch( obj.what ){
-				case "filter":
-					
-					break;
-				default:
-					return "Unknown 'what' specified in set request.";
-			}
-			break;
-		default:
-			console.log( "Unknown request received." );
-			return "Unknown request received.";
+		debug( "Argument '" + arg + "' is invalid." );
 	}
 }
+
+function main( ){
+	debug( "Starting HTTP server on port " + httpPort );
+
+	httpServer = http.createServer( function(req,res){
+		var path = url.parse(req.url).pathname;
+
+		// No file specified - shove to view.html
+		if( path == '/' ){
+			var path = '/view.html';
+			fs.readFile(__dirname+path,function(err,data){
+				if(err){ return send404(res); };
+				sendFile( res, data, 'text/html' );
+			} );
+
+		// html files..
+		}else if ( path.match( /\.html$/ ) ){
+			fs.readFile(__dirname+path,function(err,data){
+				if(err){ return send404(res); };
+				sendFile( res, data, 'text/html' );
+			} );
+
+		// javascript files..
+		}else if ( path.match( /\.js$/ ) ){
+			fs.readFile(__dirname+path,function(err,data){
+				if(err){ return send404(res); };
+			sendFile( res, data, 'text/javascript' );
+		} );
+
+		// not found.
+		}else{
+			send404(res);
+		};
+	} );
+
+	function sendFile( res, msg, contentType ){
+		res.writeHead( 200, {'Content-Type': contentType} );
+		res.write( msg );
+		res.end();
+	}
+
+	function send404( res ){
+		res.writeHead(404);
+		res.write('404');
+		res.end();
+	}
+
+	httpServer.listen(httpPort);
+
+
+	debug( "Starting up socket.io.." );
+	ioInstance = io.listen( httpServer );
+
+	debug( "Starting pcap sesion on interface '" + interface + "' with filter '" + filter + "'" );
+	pcapSession = pcap.createSession( interface, filter );
+
+} // end of main
